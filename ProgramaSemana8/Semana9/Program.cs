df33f674d789ab970e92864c15706a11a09cd39b
Christian-Cyber577 <70115908+Christopher414704@users.noreply.github.com>
2024-04-11 20:17:12 -0600

    Add files via upload

---
+++ ProgramaSemana8/Semana9/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace correo
+{
+
+    class Program
+    {
+
+        public static void Main(string[]args)
+        {
+            string Texto = "Hola Kevin, Este es mi nuevo correo: antoniocristofer577@gmail.com , Enviame los documentos faltantes";
+
+            string PatronIdentificacion = @"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b";
+
+            Regex regex = new Regex(PatronIdentificacion);
+
+            MatchCollection coincidir = regex.Matches(Texto);
+
+            foreach(Match match in coincidir){
+                Console.WriteLine(match.Value);
+            }
+
+        }
+
+
+
+    }
+
+
+
+
+}

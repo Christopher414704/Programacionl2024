6ee1d0365eb31ed7780637cea4a6e1fc9d593a84
Christian-Cyber577 <70115908+Christopher414704@users.noreply.github.com>
2024-04-18 21:07:07 -0600

    Add files via upload

---
+++ WindowsForms Semana9/Semana9WindowsForms/Semana9WindowsForms/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Semana9WindowsForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button3 = new Button();
+            label1 = new Label();
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            listBox1 = new ListBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // button3
+            // 
+            button3.Location = new Point(99, 292);
+            button3.Name = "button3";
+            button3.Size = new Size(244, 29);
+            button3.TabIndex = 2;
+            button3.Text = "Click Aqui";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(355, 47);
+            label1.Name = "label1";
+            label1.Size = new Size(92, 20);
+            label1.TabIndex = 3;
+            label1.Text = "Hola Mundo";
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Items.AddRange(new object[] { "Item 1", "Item 2", "Item 3" });
+            listBox1.Location = new Point(320, 82);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(156, 144);
+            listBox1.TabIndex = 4;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(431, 292);
+            button1.Name = "button1";
+            button1.Size = new Size(244, 29);
+            button1.TabIndex = 5;
+            button1.Text = "Interno";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(listBox1);
+            Controls.Add(label1);
+            Controls.Add(button3);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button3;
+        private Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private ListBox listBox1;
+        private Button button1;
+    }
+}
---
+++ WindowsForms Semana9/Semana9WindowsForms/Semana9WindowsForms/Form1.cs
@@ -0,0 +1,30 @@
+namespace Semana9WindowsForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
